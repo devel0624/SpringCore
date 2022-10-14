4145a84a76763716404c934a7653358834924d69
Lee Seong Jun <devel0624@gmail.com>
2022-10-14 14:29:28 +0900

    edit main

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
@@ -1,10 +1,9 @@
-package com.nhnacademy.edu.springframework.messagesender.mainclasses;
+package com.nhnacademy.edu.springframework.messagesender;
 
-import com.nhnacademy.edu.springframework.messagesender.MessageSendService;
 import com.nhnacademy.edu.springframework.messagesender.service.EmailMessageSender;
 import com.nhnacademy.edu.springframework.messagesender.service.SmsMessageSender;
 
-public class MainClass {
+public class Main {
     public static void main(String[] args) {
         new MessageSendService(new EmailMessageSender()).doSendMessage();
         new MessageSendService(new SmsMessageSender()).doSendMessage();
