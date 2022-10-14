790b9381fb544ee3814df34c9c17ae06b7ee2e48
Lee Seong Jun <devel0624@gmail.com>
2022-10-14 16:02:35 +0900

    Practice setter injection

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSendService.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSendService.java
@@ -4,7 +4,17 @@ import com.nhnacademy.edu.springframework.messagesender.User;
 import com.nhnacademy.edu.springframework.messagesender.service.MessageSender;
 
 public class MessageSendService {
-    private final MessageSender messageSender;
+    private MessageSender messageSender; // final 키워드는 생성 이후 재할당이 불가능하기 때문에 삭제
+
+    public MessageSendService(){
+        // 기본 생성자 필요, 기본 생성자만 존재하면 생략 가능
+    }
+
+    public void setMessageSender(MessageSender messageSender) {
+        System.out.println("set Message Sender in MessageSend Service");
+        this.messageSender = messageSender;
+    }
+
     public MessageSendService(MessageSender messageSender) {
         this.messageSender = messageSender;
     }
